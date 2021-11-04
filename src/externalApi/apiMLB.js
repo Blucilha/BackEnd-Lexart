ac8744309a6c8d6a6b28690b7855e82711fc1880
Blucilha <blucilha.nascimento@gmail.com>
2021-11-04 10:56:47 -0300

    Finalizando api externa

--- src/externalApi/apiMLB.js
+++ src/externalApi/apiMLB.js
@@ -3,9 +3,12 @@ const cheerio = require('cheerio');
 
 const itemsApi = async (item) => {
     const url = `https://api.mercadolibre.com/sites/MLB/search?q=${ item }`;
+    const urlScrapWeb = `https://lista.mercadolivre.com.br/${ item }`;
 
     const { data } = await axios.get(url);
-    const $ = cheerio.load(data);
+    const site = await axios.get(urlScrapWeb);
+
+    const $ = cheerio.load(site.data);
 
     const category = $('ol > li:nth-child(1) > a > span').text();
 
